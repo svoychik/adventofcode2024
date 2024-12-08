d89f11b3e80a94a7640f260016a66733f68a3c81
Serhii Voichyk <svoychik@gmail.com>
2024-12-08 13:59:32 +0100

    Solved day6

--- src/Day6pt2.java
+++ src/Day6pt2.java
@@ -14,29 +14,22 @@ public class Day6pt2 {
         for (int y = 0; y < field.rows; y++) {
             for (int x = 0; x < field.cols; x++) {
                 var position = new Day6.Position(x, y);
-//                System.out.println("New position: " + position);
-                
                 if (position.equals(field.guardPos) || field.obstacles.contains(position)) {
                     continue;
                 }
                 // Temporarily place an obstruction
                 field.obstacles.add(position);
 
-                // Check if placing the obstruction causes a loop
                 if (causesLoop(field)) {
-                    
                     validObstructionCount++;
                     System.out.println("Curr number of obstructions " + validObstructionCount);
-                    
                 }
 
-                // Remove the obstruction
                 field.guardPos = guardInitPosition;
                 field.guardDirection = guardInitDirection;
                 field.obstacles.remove(position);
             }
         }
-
         System.out.println("Number of positions causing a loop: " + validObstructionCount);
     }
 
@@ -44,16 +37,18 @@ public class Day6pt2 {
         // remember that there maybe a situation when guard visited specific position but has a different direction
         var visited = new HashSet<String>();
         while (Day6.IsInTheFiled(field.guardPos, field)) {
+            
             var nextPosition = Day6.calculateNextPosition(field);
-            if (field.obstacles.contains(nextPosition)) {
+            // next position may contain obstacle, so we have to search for next position that doesn't contain obstacle
+            while (field.obstacles.contains(nextPosition)) {
                 field.guardDirection = switch (field.guardDirection) {
                     case Up -> Day6.Direction.Right;
                     case Right -> Day6.Direction.Down;
                     case Left -> Day6.Direction.Up;
                     case Down -> Day6.Direction.Left;
                 };
+                nextPosition = Day6.calculateNextPosition(field);    
             }
-            nextPosition = Day6.calculateNextPosition(field);
             field.guardPos = nextPosition;
             var state = getUniqueState(field.guardPos, field.guardDirection);
             if(visited.contains(state)) {
