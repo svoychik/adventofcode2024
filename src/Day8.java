ac199dca0a0d9f568d90c856cc6a016dd562e3db
Serhii Voichyk <svoychik@gmail.com>
2024-12-08 14:55:02 +0100

    Day 8

---
+++ src/Day8.java
@@ -0,0 +1,85 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Day8 {
+    record Point(int i, int j) {}
+
+    public static List<String> readInput() throws Exception {
+        var input = Files.readAllLines(Paths.get(Day8.class.getResource("/day8.txt").toURI()));
+        return input.stream()
+                .filter(line -> !line.trim().isEmpty())
+                .toList();
+    }
+
+    public static char[][] buildMap(List<String> lines) {
+        int rows = lines.size();
+        int cols = lines.get(0).length();
+        char[][] map = new char[rows][cols];
+
+        for (int r = 0; r < rows; r++) {
+            char[] arr = lines.get(r).toCharArray();
+            for (int c = 0; c < cols; c++) {
+                map[r][c] = arr[c];
+            }
+        }
+        return map;
+    }
+
+    public static Map<Character, List<Point>> parseAntennas(char[][] map) {
+        Map<Character, List<Point>> antennasByFreq = new HashMap<>();
+        int rows = map.length;
+        int cols = map[0].length;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                char ch = map[r][c];
+                if (ch != '.') {
+                    antennasByFreq.computeIfAbsent(ch, k -> new ArrayList<>()).add(new Point(r, c));
+                }
+            }
+        }
+        return antennasByFreq;
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<String> lines = readInput();
+        char[][] map = buildMap(lines);
+        Map<Character, List<Point>> antennasByFreq = parseAntennas(map);
+
+        int rows = map.length;
+        int cols = map[0].length;
+
+        Set<String> antinodes = new HashSet<>();
+
+        for (Map.Entry<Character, List<Point>> entry : antennasByFreq.entrySet()) {
+            List<Point> ants = entry.getValue();
+            int n = ants.size();
+            for (int i = 0; i < n; i++) {
+                Point p1 = ants.get(i);
+                int r1 = p1.i();
+                int c1 = p1.j();
+                for (int j = i + 1; j < n; j++) {
+                    Point p2 = ants.get(j);
+                    int r2 = p2.i();
+                    int c2 = p2.j();
+
+                    // Part 1 Antinode calculation
+                    int ar1 = 2 * r1 - r2;
+                    int ac1 = 2 * c1 - c2;
+                    int ar2 = 2 * r2 - r1;
+                    int ac2 = 2 * c2 - c1;
+
+                    if (ar1 >= 0 && ar1 < rows && ac1 >= 0 && ac1 < cols) {
+                        antinodes.add(ar1 + "," + ac1);
+                    }
+                    if (ar2 >= 0 && ar2 < rows && ac2 >= 0 && ac2 < cols) {
+                        antinodes.add(ar2 + "," + ac2);
+                    }
+                }
+            }
+        }
+
+        System.out.println(antinodes.size());
+    }
+}
---
+++ src/Day8Pt2.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Day8Pt2 {
+    
+    private static int gcd(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<String> lines = Day8.readInput();
+        char[][] map = Day8.buildMap(lines);
+        Map<Character, List<Day8.Point>> antennasByFreq = Day8.parseAntennas(map);
+
+        int rows = map.length;
+        int cols = map[0].length;
+
+        Set<Day8.Point> antinodes = new HashSet<>();
+
+        // For each pair of antennas with the same frequency, find all points on the line
+        for (Map.Entry<Character, List<Day8.Point>> entry : antennasByFreq.entrySet()) {
+            List<Day8.Point> ants = entry.getValue();
+            int n = ants.size();
+            if (n < 2) continue;
+
+            for (int i = 0; i < n; i++) {
+                Day8.Point p1 = ants.get(i);
+                int r1 = p1.i();
+                int c1 = p1.j();
+                for (int j = i + 1; j < n; j++) {
+                    Day8.Point p2 = ants.get(j);
+                    int r2 = p2.i();
+                    int c2 = p2.j();
+
+                    int dy = r2 - r1;
+                    int dx = c2 - c1;
+                    int g = gcd(dy, dx);
+                    dy /= g;
+                    dx /= g;
+
+                    // Move forward direction
+                    int rr = r1;
+                    int cc = c1;
+                    while (rr >= 0 && rr < rows && cc >= 0 && cc < cols) {
+                        antinodes.add(new Day8.Point(rr, cc));
+                        rr += dy;
+                        cc += dx;
+                    }
+
+                    // Move backward direction
+                    rr = r1 - dy;
+                    cc = c1 - dx;
+                    while (rr >= 0 && rr < rows && cc >= 0 && cc < cols) {
+                        antinodes.add(new Day8.Point(rr, cc));
+                        rr -= dy;
+                        cc -= dx;
+                    }
+                }
+            }
+        }
+
+        System.out.println(antinodes.size());
+    }
+}
